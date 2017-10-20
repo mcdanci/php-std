90872227898b1cee10513ba5e0afbec69f41af2c
McDanci <mcdanci@users.noreply.github.com>
2017-10-20 11:52:57 +0800

    Config

---
+++ application/api/config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @copyright since 11:37 20/10/2017
+ * @author <mcdanci@users.noreply.github.com>
+ */
+return [
+    'default_return_type' => 'json',
+];
--- public/index.php
+++ public/index.php
@@ -4,7 +4,5 @@
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // | Author: liu21st <liu21st@gmail.com>
 
-// 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
-// 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
