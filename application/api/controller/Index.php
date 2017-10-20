3d301c600f8b12c617fb84e4ecbc55940a29d25d
McDanci <mcdanci@users.noreply.github.com>
2017-10-20 14:59:43 +0800

    Update

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -3,13 +3,14 @@
  * @copyright since 10:53 20/10/2017
  * @author <mcdanci@users.noreply.github.com>
  */
-namespace app\index\controller;
+namespace app\api\controller;
 
-use think\controller\Rest;
+use think\Controller;
 
-class Index extends Rest
+class Index extends Controller
 {
     public function index()
     {
+        return [];
     }
 }
