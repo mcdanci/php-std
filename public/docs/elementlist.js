e7857069ef22feaf60995197a72f401c9f803ff3
McDanci <mcdanci@users.noreply.github.com>
2017-10-21 11:46:47 +0800

    Update

--- public/docs/elementlist.js
+++ public/docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","app\\api\\controller\\Controller"],["c","app\\api\\controller\\Index"]];
+ApiGen.elements = [["c","app\\api\\controller\\Controller"],["c","app\\api\\controller\\Index"],["c","app\\api\\controller\\Registration"]];
