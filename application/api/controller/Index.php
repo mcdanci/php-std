bb4d624326c9637ca7d20ef62a88b47d1a64c40f
McDanci <mcdanci@users.noreply.github.com>
2017-10-21 10:38:12 +0800

    Update

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -5,6 +5,9 @@
  */
 namespace app\api\controller;
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 class Index extends Controller
 {
     /**
@@ -46,5 +49,59 @@ class Index extends Controller
             'info' => 'OK',
             'body' => $data,
         ];
+    }
+
+    /**
+     * @return array
+     * @link http://www.thinkphp.cn/topic/43753.html
+     */
+    public function dbgSendMail($email_addr)
+    {
+        static $mailerEmailAddr = 'web@fmnii.com';
+
+        // Begin
+        $mail = new PHPMailer(true);
+
+        try {
+            // Server settings
+            $mail->SMTPDebug = 2;
+            $mail->isSMTP();
+            $mail->Host = 'smtp.exmail.qq.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = $mailerEmailAddr;
+            $mail->Password = '@Fmnii789';
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
+
+            // Recipients
+            $mail->setFrom($mailerEmailAddr, 'Mailer');
+            $mail->addAddress($email_addr, 'Recipient');
+            $mail->addReplyTo($mailerEmailAddr, 'Reply to Address');
+            $mail->addCC($email_addr);
+            $mail->addBCC($email_addr);
+
+            // Attachments
+            $mail->addAttachment(__FILE__, 'Yours.php');
+
+            // Content
+            $mail->isHTML(true);
+            $mail->Subject = 'Fmnii S Show Subject';
+            $mail->Body = '<h1>Hello, buddy</h1>';
+            $mail->AltBody = 'Guy!';
+
+            $mail->send();
+
+            return [
+                'status' => 200,
+                'info' => 'Message has been sent',
+                'body' => $mail,
+            ];
+        } catch (Exception $e) {
+            return [
+                'status' => 500,
+                'info' => 'Message could note be sent',
+                'body' => 'Mailer error: ' . $mail->ErrorInfo,
+            ];
+        }
     }
 }
