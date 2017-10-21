1aaa49fadf4c995a5f3aa9c8a04ace204f2716ce
McDanci <mcdanci@users.noreply.github.com>
2017-10-21 08:58:58 +0800

    Update

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -9,8 +9,27 @@ use think\Controller;
 
 class Index extends Controller
 {
+    /**
+     * 跨域 header
+     * @param false|string $domainName 可信域
+     */
+    protected static function setHeaders($domainName = false)
+    {
+        // 限定式跨域
+        if ($domainName) {
+            header('Access-Control-Allow-Origin: ' . $domainName);
+            header('Access-Control-Allow-Credentials: true'); // 存取许可
+        } else {
+            header('Access-Control-Allow-Origin: *');
+        }
+
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+        header('Access-Control-Max-Age: 3628800'); // TODO
+    }
+
     public function index()
     {
+        return 'S Show Server';
     }
 
     public function listIso3166()
@@ -22,5 +41,10 @@ class Index extends Controller
         }
 
         return $data;
+    }
+
+    protected function _initialize()
+    {
+        self::setHeaders();
     }
 }
---
+++ application/api/database.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @copyright since 18:03 20/10/2017
+ * @author <mcdanci@users.noreply.github.com>
+ */
+return [
+    'prefix' => 'ss_reg_',
+];
