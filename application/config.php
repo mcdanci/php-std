df52b4fa87987d44f27b73573800e30e7692269b
McDanci <mcdanci@users.noreply.github.com>
2017-10-24 12:02:11 +0800

    Update

--- application/config.php
+++ application/config.php
@@ -1,13 +1,19 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
+/**
+ * ThinkPHP
+ *
+ * *WE CAN DO IT JUST THINK*
+ *
+ * [Licensed](http://www.apache.org/licenses/LICENSE-2.0)
+ *
+ * @copyright http://thinkphp.cn/, 2006 ~ 2016, all rights reserved.
+ * @author 流年 (liu21st) <liu21st@gmail.com>
+ * @author yunwuxin <448901948@qq.com>
+ * @author <outieyi@milianjie.com>
+ */
 
 return [
-    //'app_trace' => true,
+    //'app_trace' => true, // TODO: for debug
 
 
     'app_namespace' => 'app',
