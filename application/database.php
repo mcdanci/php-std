cee34adc1f2e34dc7b054c359792642317362ec5
McDanci <mcdanci@users.noreply.github.com>
2017-10-27 13:59:19 +0800

    Update

--- application/database.php
+++ application/database.php
@@ -1,41 +1,34 @@
 <?php
 return [
-    'type' => 'mysql',
+    // TODO: development environment
+    'debug' => true,
+    'sql_explain' => true,
 
-    // TODO: for dev
+    //region development environment
+
+    // TODO: intranet
     'hostname' => 'develop-1.fmnii.e13.cc',
-    'database' => 's-show',
     'username' => 's-show',
     'password' => 'bqqcvALY6sE3Xc5j',
+    'database' => 's-show',
 
+    // TODO: Internet
     //'hostname' => '39.108.221.196',
-    //'database' => 'sshowapi',
     //'username' => 'sshow',
     //'password' => 'sshowadmin888',
+    //'database' => 'sshowapi',
 
-    // 连接dsn
-    'dsn' => '',
-    // 数据库连接参数
-    'params' => [],
+    //endregion
+
+    //region Project
+
+    'prefix' => 'ss_reg_',
+
+    //endregion
+
+    //region Common
 
     'charset' => 'utf8mb4',
-    'prefix' => 'ss_reg_',
-    'debug' => true,
-    'deploy' => 0,
-    'rw_separate' => false,
 
-    // 读写分离后 主服务器数量
-    'master_num' => 1,
-    // 指定从服务器序号
-    'slave_no' => '',
-    // 是否严格检查字段是否存在
-    'fields_strict' => true,
-    // 数据集返回类型
-    'resultset_type' => 'array',
-    // 自动写入时间戳字段
-    'auto_timestamp' => false,
-    // 时间字段取出后的默认时间格式
-    'datetime_format' => 'Y-m-d H:i:s',
-    // 是否需要进行SQL性能分析
-    'sql_explain' => false,
+    //endregion
 ];
