e217b66697efa286b42906cc82c5724c4cea64a9
McDanci <mcdanci@users.noreply.github.com>
2017-10-28 10:22:21 +0800

    Update

---
+++ extend/Fmnii/Controller/Convention.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * @copyright since 9:51 28/10/2017
+ * @author <mcdanci@users.noreply.github.com>
+ */
+namespace Fmnii\Controller;
+
+/**
+ * Fmnii Controller Convention
+ *
+ * # Status Code
+ * Value | Meaning (`info`rmation)
+ * --- | ---
+ * 1 | Succeeded
+ * 0 | Failed
+ * @package Fmnii\Controller
+ */
+trait Convention
+{
+    /**
+     * @var int Succeeded
+     */
+    protected static $scSucceeded = 1;
+
+    /**
+     * @var int Failed
+     */
+    protected static $scFailed = 0;
+
+    /**
+     * Template for message returned
+     * @param int $statusCode
+     * @param null|string $statusMessage
+     * @param null|array $body
+     * @return array
+     * @return ->status Status code
+     * @return ->info *optional* Status message
+     * @return ->... *optional* Message body
+     * @throws \Exception
+     */
+    protected static function retTemp($statusCode = 1, $statusMessage = null, $body = null)
+    {
+        $ret = [];
+
+        // Status code
+        if (in_array($statusCode, [self::$scSucceeded, self::$scFailed])) {
+            $ret['status'] = $statusCode;
+        } else {
+            throw new \Exception('Status code is not in range');
+        }
+
+        // Status message
+        if (is_string($statusMessage)) {
+            $ret['info'] = $statusMessage;
+        }
+
+        /**
+         * Message body set
+         */
+        if (is_array($body)) {
+            // Check if `status` or `info` is in `body` or not
+            $statusKeyWithinBody = [];
+
+            foreach (['status', 'info'] as &$key2bChecked) {
+                if (array_key_exists($key2bChecked, $body)) {
+                    $statusKeyWithinBody[] = $key2bChecked;
+                }
+            }
+
+            if ($statusKeyWithinBody) {
+                $eDesc = 'Message body with key ' . implode(' and ', $statusKeyWithinBody);
+                throw new \Exception($eDesc);
+            } else {
+                $ret = array_merge($ret, $body);
+            }
+        }
+
+        return $ret;
+    }
+}
