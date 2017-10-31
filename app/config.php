300578ac6259b19ee248b33108ce6685f9d31fb7
McDanci <mcdanci@users.noreply.github.com>
2017-10-31 15:06:56 +0800

    Update

--- app/config.php
+++ app/config.php
@@ -14,28 +14,13 @@
 
 // TODO
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // | Author: 流年 <liu21st@gmail.com>
+// | Author: yunwuxin <448901948@qq.com>
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// | Author: yunwuxin <448901948@qq.com>
 
 return [
     'app_trace' => true, // TODO: for debug
-
-    // descriptions of categories
-    'category_desc' => [
-        1 => 'material',
-        2 => 'process / component',
-        3 => 'machinery',
-        4 => 'device / tools',
-        5 => 'creation technology',
-        6 => 'manufacture consultation',
-    ],
-
 
     'app_namespace' => APP_NAMESPACE,
 
