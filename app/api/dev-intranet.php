b9cf685580660ceda7cf565f661e829968dc8729
McDanci <mcdanci@users.noreply.github.com>
2017-10-31 15:11:33 +0800

    Update

--- app/api/dev-intranet.php
+++ app/api/dev-intranet.php
@@ -3,11 +3,14 @@
  * @copyright since 9:17 28/10/2017
  * @author <mcdanci@users.noreply.github.com>
  */
+
+$confDB = [
+    'hostname' => 'develop-1.fmnii.e13.cc',
+    'username' => 's-show',
+    'password' => 'bqqcvALY6sE3Xc5j',
+    'database' => 's-show',
+];
+
 return [
-    'database' => [
-        'hostname' => 'develop-1.fmnii.e13.cc',
-        'username' => 's-show',
-        'password' => 'bqqcvALY6sE3Xc5j',
-        'database' => 's-show',
-    ],
+    'database' => array_merge(\think\Config::get('database'), $confDB),
 ];
