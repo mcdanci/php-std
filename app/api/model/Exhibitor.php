52e8d72928db837952f61221279d06e72324b29e
McDanci <mcdanci@users.noreply.github.com>
2017-10-31 15:15:30 +0800

    Update

---
+++ app/api/model/Exhibitor.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright since 15:14 31/10/2017
+ * @author <mcdanci@users.noreply.github.com>
+ */
+namespace app\api\model;
+
+class Exhibitor extends Registrant
+{
+}
---
+++ app/api/model/Visitor.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright since 15:12 31/10/2017
+ * @author <mcdanci@users.noreply.github.com>
+ */
+namespace app\api\model;
+
+/**
+ * Visitor
+ * @package app\api\model
+ */
+class Visitor extends Registrant
+{
+}
