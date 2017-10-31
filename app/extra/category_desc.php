fea220d06c321ee2bdcb181be5b3b0843bb84ccf
McDanci <mcdanci@users.noreply.github.com>
2017-10-31 15:05:45 +0800

    Update

---
+++ app/extra/category_desc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright since 15:01 31/10/2017
+ * @author <mcdanci@users.noreply.github.com>
+ */
+
+// descriptions of categories
+return [
+    1 => 'material',
+    2 => 'process / component',
+    3 => 'machinery',
+    4 => 'device / tools',
+    5 => 'creation technology',
+    6 => 'manufacture consultation',
+];
