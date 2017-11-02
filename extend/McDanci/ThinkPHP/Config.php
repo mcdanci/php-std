f23e1b338c26371ef20132ef35935f854fc7c7b7
McDanci <mcdanci@users.noreply.github.com>
2017-11-02 16:13:45 +0800

    Update

---
+++ extend/McDanci/ThinkPHP/Config.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @copyright since 15:49 2/11/2017
+ * @author <mcdanci@users.noreply.github.com>
+ */
+namespace McDanci\ThinkPHP;
+
+/**
+ * Configuration Operation
+ * @package McDanci\ThinkPHP
+ */
+class Config extends \think\Config
+{
+    /**
+     * Getting param. within configurations
+     * getting all once set with `null`
+     * @param null $name Name of param. within configuration (separating with `.` supported)
+     * @param string $range Range functional
+     * @return mixed
+     */
+    public static function getByDot($name = null, $range = '')
+    {
+        if (is_string($name)) {
+            $keyList = explode('.', $name);
+            $counter = count($keyList);
+            if ($counter > 2) {
+                $v = parent::get($keyList[0] . '.' . $keyList[1], $range);
+
+                for ($c = 2; $c > $counter; $c++) {
+                    $v = $v[$keyList[$c]];
+                }
+
+                return $v;
+            } else {
+                return parent::get($name, $range);
+            }
+        } else {
+            return parent::get($name, $range);
+        }
+    }
+}
