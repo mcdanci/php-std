baafcacf8199fe49dc19c15f67e1d9f83e650d8c
McDanci <mcdanci@users.noreply.github.com>
2017-11-02 14:19:20 +0800

    Update

---
+++ tests/api/controller/IndexTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @copyright since 10:55 2/11/2017
+ * @author <mcdanci@users.noreply.github.com>
+ */
+namespace tests\api\controller;
+
+use tests\TestCase;
+
+class IndexTest extends TestCase
+{
+    /**
+     * @todo
+     */
+    public function testIndex()
+    {
+    }
+}
