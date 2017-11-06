9884acc9d045fab94ee5db6da1f9a51901ef23d0
McDanci <mcdanci@users.noreply.github.com>
2017-11-06 10:44:23 +0800

    ISO3166

--- app/api/controller/Index.php
+++ app/api/controller/Index.php
@@ -13,16 +13,47 @@ use McDanci\ThinkPHP\Config;
  */
 class Index extends Controller
 {
+    //region ISO 3166
+
     /**
-     * S Show API Echo
-     * @return array
-     * ->**status** int
-     * @throws \Exception
+     * Process for the list of ISO 3166
+     * - Description of Taiwan
+     * @param $list
      * @todo Unit test
      */
-    public function index()
+    private static function modCountriesNRegions(&$list)
     {
-        return self::retTemp();
+        static $NUMERIC = '158';
+
+        if (array_key_exists($NUMERIC, $list)) {
+            $desc = explode(' ', $list[$NUMERIC]);
+            $list[$NUMERIC] = reset($desc);
+        }
+    }
+
+    /**
+     * Getting data as list of ISO 3166
+     * @return array
+     * ->**status** int
+     *
+     * ->**body** array
+     *
+     * ->->**numeric**:**name** string Entry
+     * @todo Unit test
+     */
+    public function listOptionIso3166Nov()
+    {
+        $list = [];
+
+        $listOriginal = (new \League\ISO3166\ISO3166)->all();
+
+        foreach ($listOriginal as &$entry) {
+            $list[$entry['numeric']] = $entry['name'];
+        }
+
+        self::modCountriesNRegions($list);
+
+        return ['status' => self::$scOK, 'body' => $list];
     }
 
     /**
@@ -34,20 +65,20 @@ class Index extends Controller
      *
      * ->**body** array
      *
-     * ->-> array Tuple
+     * ->-> array
      *
      * ->->->**name** string
      *
      * ->->->**numeric** string
-     * @todo Unit test
+     * @deprecated
      */
     public function listOptionIso3166()
     {
         $data = (new \League\ISO3166\ISO3166)->all();
 
-        foreach ($data as &$area) {
+        foreach ($data as &$cnr) {
             foreach (['alpha2', 'alpha3', 'currency'] as &$key) {
-                unset($area[$key]);
+                unset($cnr[$key]);
             }
         }
 
@@ -55,13 +86,17 @@ class Index extends Controller
     }
 
     /**
+     * Getting data as list of ISO 3166
      * @return array
+     * @see \app\api\controller\Index::listOptionIso3166
      * @deprecated
      */
     public function listIso3166()
     {
         return $this->listOptionIso3166();
     }
+
+    //endregion
 
     /**
      * Getting data as list of gender
@@ -72,7 +107,7 @@ class Index extends Controller
      *
      * ->**body** array
      *
-     * ->-> array Tuple in `value: HTML`
+     * ->->**value**:**HTML** array Entry
      * @throws \Exception
      * @todo Unit test
      */
