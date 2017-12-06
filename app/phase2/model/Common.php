79f9320ed39fada4169a11145863dce1abee7b4a
McDanci <mcdanci@users.noreply.github.com>
2017-12-06 14:59:30 +0800

    Update

--- app/phase2/model/Common.php
+++ app/phase2/model/Common.php
@@ -18,4 +18,14 @@ h1 {font-size: 100px; font-weight: normal; margin-bottom: 12px; }
 p {line-height: 1.6em; font-size: 42px}
 </style>
 <div style="padding: 24px 48px;"><h1>:)</h1> S Show API, phase 2</div>';
+
+    /**
+     * Encrypt password.
+     * @param $original
+     * @return bool|string
+     */
+    public static function encryptPassword($original)
+    {
+        return password_hash($original, PASSWORD_DEFAULT, ['cost' => 8]);
+    }
 }
