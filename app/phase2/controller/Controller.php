041df58f548c004e918d54cd490fe7e690f74bab
McDanci <mcdanci@users.noreply.github.com>
2017-12-07 13:58:27 +0800

    Update

---
+++ app/phase2/controller/Controller.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @copyright since 13:48 7/12/2017
+ * @author <mcdanci@users.noreply.github.com>
+ */
+namespace app\phase2\controller;
+
+use think\Auth;
+
+class Controller extends \think\Controller
+{
+    /**
+     * @var Auth
+     */
+    protected $auth;
+
+    public function _initialize()
+    {
+        parent::_initialize();
+
+        $this->auth = new Auth();
+
+        // TODO: controller-action, session uid
+        if (!$this->auth->check($this->request->controller() . '-' . $this->request->action(), session('uid'))) {
+            $this->error('You have no right to access.');
+        }
+    }
+}
