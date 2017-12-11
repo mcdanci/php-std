b8fded6b0380e0e486d08f544dfebd831f5f94b0
McDanci <mcdanci@users.noreply.github.com>
2017-12-11 17:37:03 +0800

    Update

--- app/phase2/controller/Signing.php
+++ app/phase2/controller/Signing.php
@@ -56,10 +56,53 @@ class Signing extends Controller
 
     //region Debug
 
+    /**
+     * Boolean value of PayPal
+     */
+    const
+        PAYPAL_FALSE = '0',
+        PAYPAL_TRUE = '1';
+
+    /**
+     * @return mixed
+     * @todo 4 debug
+     */
     public function payDebug()
     {
         Config::set(RETURN_TYPE_DEFAULT, 'html');
+
         $this->assign([
+            //'option_list' => [
+            //    'quantity',
+            //    'item_number',
+            //    'shipping',
+            //    'shipping2',
+            //    'handling',
+            //    'tax',
+            //
+            //    'no_shipping',
+            //
+            //    'cn',
+            //    'no_note',
+            //
+            //    'on0',
+            //    'os0',
+            //
+            //    'on1',
+            //    'os1',
+            //
+            //    'custom',
+            //    'invoice',
+            //
+            //    'notify_url',
+            //    'return',
+            //    'cancel_return',
+            //
+            //    'image_url',
+            //    'cs',
+            //],
+
+            // Required
             'business' => 'east.usa@fmnii.com',
             'item_name' => 'S Show Transaction',
             'currency_code' => 'USD',
@@ -68,7 +111,81 @@ class Signing extends Controller
                 'src' => 'https://www.paypal.com/zh_XC/i/btn/x-click-but01.gif',
                 'alt' => 'Make payments with PayPal - it\'s fast, free and secure!',
             ],
+
+
+            // TODO optional
+            // Available variable list
+            //'quantity' => 1,
+            //'item_number' => '',
+            //'shipping' => 0.0,
+            //'shipping2' => 0.0,
+            //'handling' => 0.0,
+            //'tax' => 0,
+            //
+            //'no_shipping' => self::PAYPAL_FALSE,
+            //
+            'cn' => 'Under debug',
+            //'no_note' => self::PAYPAL_FALSE,
+            //
+            //'on0' => '',
+            //'os0' => '',
+            //
+            //'on1' => '',
+            //'os1' => '',
+            //
+            //'custom' => null,
+            //'invoice' => null,
+            //
+            //'notify_url' => '',
+            //'return' => '',
+            //'cancel_return' => '',
+            //
+            'image_url' => 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png', // TODO: 4 debug
+            //'cs' => self::PAYPAL_FALSE,
+
+
+            // Configuration
+            'var_more' => false,
+            'cmd_cart' => false,
+
+
+            'email' => '',
+
+            'first_name' => '',
+            'last_name' => '',
+
+            'address1' => '',
+            'address2' => '',
+            'city' => '',
+            'state' => '',
+            'zip' => null,
+
+            'night_phone_a' => 0,
+            'night_phone_b' => 0,
+
+            'day_phone_a' => 0,
+            'day_phone_b' => 0,
+
+
+            'list_item' => [
+                [
+                    'item_name' => '',
+                    'item_number' => '',
+                    'amount' => 0.0,
+                    'shipping' => 0.0,
+                    'shipping2' => 0.0,
+                    'handling' => 0.0,
+
+                    'on0' => '',
+                    'os0' => '',
+
+                    'on1' => '',
+                    'os1' => '',
+                ],
+            ],
+
         ]);
+
         return $this->display('_tmp/payment');
     }
 
--- app/phase2/view/_tmp/payment.tpl
+++ app/phase2/view/_tmp/payment.tpl
@@ -1,8 +1,36 @@
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-    <input type="hidden" name="cmd" value="_xclick">
+    <{if $var_more}>
+        <input type="hidden" name="cmd" value="_ext-enter">
+        <input type="hidden" name="redirect_cmd" value="_xclick">
+    <{elseif $cmd_cart}>
+        <input type="hidden" name="cmd" value="_cart">
+        <input type="hidden" name="upload" value="1">
+        <{foreach from=$list_item key="cart_item_offset" item="cart_item"}>
+            <{foreach from=$cart_item key="cart_item_option_key" item="cart_item_option_val"}>
+                <{* TODO: 4 debug *}>
+                <input
+                        type="hidden"
+                        name="<{$cart_item_option.cart_item_option_key + '_' + ($cart_item_offset + 1)}>"
+                        value="<{$cart_item_option_val}>"
+                >
+            <{/foreach}>
+        <{/foreach}>
+    <{else}>
+        <input type="hidden" name="cmd" value="_xclick">
+    <{/if}>
+
     <input type="hidden" name="business" value="<{$business}>">
     <input type="hidden" name="item_name" value="<{$item_name}>">
     <input type="hidden" name="currency_code" value="<{$currency_code}>">
     <input type="hidden" name="amount" value="<{$amount}>">
+
+    <{* TODO: 4 debug *}>
+    <{if isset($cn)}>
+        <input type="hidden" name="cn" value="<{$cn}>">
+    <{/if}>
+    <{if isset($image_url)}>
+        <input type="hidden" name="image_url" value="<{$image_url}>">
+    <{/if}>
+
     <input type="image" name="submit" src="<{$btn_submit_img.src}>" alt="<{$btn_submit_img.alt}>">
 </form>
