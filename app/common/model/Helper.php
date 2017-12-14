1b3b37efc65a29942b9a02ffa4699b9b93a35de9
McDanci <mcdanci@users.noreply.github.com>
2017-12-14 16:02:14 +0800

    Update

--- app/common/model/Helper.php
+++ app/common/model/Helper.php
@@ -5,18 +5,35 @@ use think\Model;
 
 class Helper extends Model
 {
+    const A_ID_OFFSET = 64;
+
     /**
      * @param int $id
      * @return bool|string
      */
     public static function getAById($id)
     {
-        if ($id >= 1 && $id <= 8) {
-            return chr(64 + $id);
-        } elseif ($id === 0) {
-            return (string)$id;
-        } else {
-            return false;
+        if (is_int($id)) {
+            if ($id >= 1 && $id <= 8) {
+                return chr(self::A_ID_OFFSET + $id);
+            } elseif ($id === 0) {
+                return (string)$id;
+            }
         }
+
+        return false;
+    }
+
+    public static function getIdByA($a)
+    {
+        if (is_string($a)) {
+            if (in_array($a, range('A', 'H'))) {
+                return ord($a) - self::A_ID_OFFSET;
+            } elseif ($a === '0') {
+                return (int)$a;
+            }
+        }
+
+        return false;
     }
 }
