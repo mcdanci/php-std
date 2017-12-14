f22cad797a9e3991ea81cebbb2e0a60147a4776b
McDanci <mcdanci@users.noreply.github.com>
2017-12-14 16:15:14 +0800

    Update

--- www/api/router.php
+++ www/api/router.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * @todo
+ */
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
@@ -11,5 +14,5 @@ if (is_file($_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI'])) {
     if (!isset($_SERVER['PATH_INFO'])) {
         $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI'];
     }
-    require __DIR__ . "/index.php";
+    require __DIR__ . '/index.php';
 }
