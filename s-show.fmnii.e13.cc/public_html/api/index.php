d707ca3a036cb04ffb2b71370f636c8356e0e25f
McDanci <mcdanci@users.noreply.github.com>
2017-12-16 12:02:12 +0800

    Update

--- s-show.fmnii.e13.cc/public_html/api/index.php
+++ s-show.fmnii.e13.cc/public_html/api/index.php
@@ -1,7 +1,7 @@
 <?php
 const APP_NAMESPACE = 'app';
 
-define('PROJECT_DIR', dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR);
+define('PROJECT_DIR', dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR);
 define('APP_PATH', PROJECT_DIR . APP_NAMESPACE . DIRECTORY_SEPARATOR);
 
 require PROJECT_DIR . 'thinkphp' . DIRECTORY_SEPARATOR . 'start.php';
