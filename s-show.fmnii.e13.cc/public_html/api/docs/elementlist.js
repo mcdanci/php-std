3f6656c6cfb16db275feb3433a019a201f48c85c
McDanci <mcdanci@users.noreply.github.com>
2017-12-20 15:38:37 +0800

    Update

--- s-show.fmnii.e13.cc/public_html/api/docs/elementlist.js
+++ s-show.fmnii.e13.cc/public_html/api/docs/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","app\\reg\\controller\\v1\\Main"]];
+ApiGen.elements = [["c","app\\backstage\\controller\\v1\\Audit"],["c","app\\backstage\\controller\\v1\\Controller"],["c","app\\backstage\\controller\\v1\\Main"],["c","app\\backstage\\controller\\v1\\SignedController"]];
