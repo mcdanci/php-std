69435da3f14b4784ae9647af36c87d2d80c6e59a
McDanci <mcdanci@users.noreply.github.com>
2017-12-26 18:14:29 +0800

    Update

--- app/backstage/controller/v1/Controller.php
+++ app/backstage/controller/v1/Controller.php
@@ -19,6 +19,6 @@ abstract class Controller extends \think\Controller
     protected function _initialize()
     {
         parent::_initialize();
-        self::setHeaders('http://localhost:8080, http://test.ershaoyes.com/'); // TODO: for debug
+        self::setHeaders(['http://localhost:8080', 'http://test.ershaoyes.com/', 'http://127.0.0.1']); // TODO: for debug
     }
 }
